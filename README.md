f7177a557abeef1d22c763d27ee3e39e55658205
DmitrenkoAlex <dmitrenkolesha@mail.ru>
2022-02-22 01:56:19 +0300

    AddTest

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/0kbmcklpqibfw5yt/branch/main?svg=true)](https://ci.appveyor.com/project/DmitrenkoAlex/debitcard/branch/main)
---
+++ src/test/java/ru/netology/SeleniumTest.java
@@ -0,0 +1,111 @@
+package ru.netology;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+
+public class SeleniumTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        //указание, что нужно использовать драйвер на локальной машине и путь к нему
+        //System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        //использование этой библиотеки позволяет скачаать драйвер для того же браузера и той же версии, но на linux для CI
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        //Включение headless режима при использовании selenium
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+
+    @Test
+    public void PositiveTest() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Гарри Поттер");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+78005553535");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String expectedMessage = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actualMessage = driver.findElement(By.cssSelector(".paragraph_theme_alfa-on-white")).getText();
+        Assertions.assertEquals(expectedMessage, actualMessage.trim());
+    }
+
+    @Test
+    public void NegativeBecauseOfTranslitName() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Harry Поттер");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+78005553535");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String expectedMessage = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actualMessage = driver.findElement(By.cssSelector("[data-test-id = name] .input__sub")).getText();
+        Assertions.assertEquals(expectedMessage, actualMessage.trim());
+    }
+
+    @Test
+    public void NegativeBecauseOfEmptyNameField() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+78005553535");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String expectedMessage = "Поле обязательно для заполнения";
+        String actualMessage = driver.findElement(By.cssSelector("[data-test-id = name] .input__sub")).getText();
+        Assertions.assertEquals(expectedMessage, actualMessage.trim());
+    }
+
+    @Test
+    public void NegativeBecauseOfIncorrectPhoneNumber() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Гарри Поттер");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("8005553535");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String expectedMessage = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actualMessage = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
+        Assertions.assertEquals(expectedMessage, actualMessage.trim());
+    }
+
+    @Test
+    public void NegativeBecauseOfEmptyPhoneNumber() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Гарри Поттер");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String expectedMessage = "Поле обязательно для заполнения";
+        String actualMessage = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
+        Assertions.assertEquals(expectedMessage, actualMessage.trim());
+    }
+
+    // Нет флажка согласия в чек-боксе
+    @Test
+    public void NegativeBecauseOfEmptyCheckBox() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Гарри Поттер");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+78005553535");
+        driver.findElement(By.cssSelector("button")).click();
+        driver.findElement(By.cssSelector(".input_invalid"));
+    }
+}
